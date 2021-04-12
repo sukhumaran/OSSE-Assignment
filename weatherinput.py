853f0c21180629459b71884bfeb4a420aab1f9dd
sukhumaran <73813652+sukhumaran@users.noreply.github.com>
2021-04-12 11:21:38 +0530

    Add files via upload

---
+++ weatherinput.py
@@ -0,0 +1,8 @@
+x = int(input('enter the temperature outside:'))
+if x < 20:
+    print('Nice cold weather')
+elif x > 25:
+    print('Nice hot weather')
+else:
+    print('it is good outside')
+print('Thank you')
